db3f894f2d2e462cea741cb00c4e8c21dfa20b16
Thiên Võ <thien.vo.tpv@one-line.com>
2025-08-19 10:55:48 +0700

    update admin account and API update role for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { Roles } from '../auth/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { UserRole } from './users.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get('me')
   @UseGuards(JwtAuthGuard)
   getProfile() {
@@ -16,5 +20,13 @@ export class UsersController {
   @Roles('admin')
   getAdminData() {
     return { message: 'Only admin can see this' };
+  }
+
+  @Post('setRole')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
+  async setRole(@Body() dto: { userId: number; role: UserRole }) {
+    await this.usersService.setRole(dto.userId, dto.role);
+    return 'Success';
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './users.entity';
+import { User, UserRole } from './users.entity';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  constructor(@InjectRepository(User) private repo: Repository<User>) {
+    this.ensureAdmin();
+  }
+
+  private async ensureAdmin() {
+    const admin = await this.repo.findOne({ where: { email: 'admin' } });
+    if (!admin) {
+      const user = this.repo.create({
+        email: 'admin',
+        password: await bcrypt.hash('admin', 10),
+        role: UserRole.ADMIN,
+      });
+      await this.repo.save(user);
+    }
+  }
+
+  async setRole(userId: number, role: UserRole) {
+    const result = await this.repo.update(userId, { role });
+    if (result.affected === 0) {
+      throw new Error('User not found');
+    }
+  }
 
   async create(email: string, password: string) {
     const hash = await bcrypt.hash(password || email, 10);
